fc5b139403c3e6e3a99f83343a270a814e2510f0
Dan Slocombe <daslocom@microsoft.com>
2022-11-01 22:04:14 +0000

    Cursor moving

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,23 @@ mod trie;
 
 use console::Term;
 
-fn main() {
-    let shell = shell::Shell::default();
+use std::io::Write;
 
-    let term = Term::stdout();
+fn main() {
+    let mut shell = shell::Shell::default();
+
+    let mut term = Term::stdout();
 
     while let Ok(k) = term.read_key() {
-        println!("{:?}", k);
+        //println!("{:?}", k);
+        let command = shell::Command::apply_key(&shell.current_prompt, k);
+        shell.apply_command(command);
+
+        _ = term.clear_line().unwrap();
+        term.write(&shell.current_prompt.text.as_bytes()).unwrap();
+
+        _ = term.move_cursor_left(usize::MAX).unwrap();
+        _ = term.move_cursor_right(shell.current_prompt.pos).unwrap();
     }
 
     println!("Hello, world!");
--- src/shell.rs
+++ src/shell.rs
@@ -1,13 +1,61 @@
 use crate::history_completer::HistoryCompleter;
+use console::Key;
 
 #[derive(Debug, Default)]
 pub struct Shell {
     global_history: HistoryCompleter,
+    pub current_prompt: Zipper,
 }
 
+impl Shell {
+    pub fn apply_command(&mut self, command: Command) {
+        match command {
+            Command::Text(z) => {
+                self.current_prompt = z;
+            }
+            _ => {}
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone)]
 pub struct Zipper {
-    text: String,
-    pos: usize,
+    pub text: String,
+    pub pos: usize,
+}
+
+impl Zipper {
+    pub fn insert(&mut self, c: char) {
+        self.text.insert(self.pos, c);
+        self.pos += 1;
+    }
+
+    pub fn cursor_left(&mut self) {
+        self.pos = self.pos.saturating_sub(1);
+    }
+
+    pub fn cursor_right(&mut self) {
+        self.pos = (self.pos + 1).min(self.text.len() - 1);
+    }
+
+    pub fn clone_insert(&self, c: char) -> Self {
+        let mut new = self.clone();
+        new.insert(c);
+        new
+    }
+}
+
+macro_rules! zipper_clone_apply {
+    ($z:expr, $f:ident) => {{
+        let mut new = $z.clone();
+        new.$f();
+        new
+    }};
+    ($z:expr, $f:ident, $a:expr) => {{
+        let mut new = $z.clone();
+        new.$f($a);
+        new
+    }};
 }
 
 pub enum Command {
@@ -25,4 +73,18 @@ pub enum Command {
     HistoryBack,
 
     Search,
+
+    NoOp,
+}
+
+impl Command {
+    pub fn apply_key(current: &Zipper, k: console::Key) -> Self {
+        match k {
+            Key::ArrowLeft => Self::Text(zipper_clone_apply!(current, cursor_left)),
+            Key::ArrowRight => Self::Text(zipper_clone_apply!(current, cursor_right)),
+            Key::Char(c) => Self::Text(zipper_clone_apply!(current, insert, c)),
+
+            _ => Self::NoOp,
+        }
+    }
 }
